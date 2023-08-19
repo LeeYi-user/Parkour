58c84048d5831713e7e98c34b182998fe3a1c28a
LeeYi-user <khes60122@gmail.com>
2023-08-19 23:30:33 +0800

    change camera

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using Cinemachine;
 
 [RequireComponent(typeof(CharacterController), typeof(Animator))]
 public class PlayerController : NetworkBehaviour
 {
-
     private CharacterController characterController;
     private Animator animator;
 
@@ -16,16 +16,25 @@ public class PlayerController : NetworkBehaviour
     private Vector3 movementDirection = Vector3.zero;
     private bool playerGrounded;
 
+    [SerializeField]
+    private CinemachineVirtualCamera playerCam;
 
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
+
+        if (IsOwner)
+        {
+            playerCam = FindObjectOfType<CinemachineVirtualCamera>();
+            playerCam.LookAt = transform;
+            playerCam.Follow = transform;
+        }
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         if (!IsOwner)
         {
@@ -36,20 +45,18 @@ public class PlayerController : NetworkBehaviour
 
         //movement
         Vector3 inputMovement = transform.forward * movementSpeed * Input.GetAxisRaw("Vertical");
+
         characterController.Move(inputMovement * Time.deltaTime);
-
-        transform.Rotate(Vector3.up * Input.GetAxisRaw("Horizontal") * rotationSpeed);
-
+        transform.Rotate(Vector3.up * Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime);
 
         //jumping
         if (Input.GetButton("Jump") && playerGrounded)
         {
             movementDirection.y = jumpSpeed;
         }
+
         movementDirection.y -= gravity * Time.deltaTime;
-
         characterController.Move(movementDirection * Time.deltaTime);
-
 
         //animations
         animator.SetBool("isRunning", Input.GetAxisRaw("Vertical") != 0);
